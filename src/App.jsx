0b8c82fa549a48ef37b1864894c37a7ca1d3b741
WJStephenson <willjosteph@gmail.com>
2023-08-09 20:03:09 +0100

    added routing, search and about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './App.css'
 import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
 import Homepage from './pages/Homepage/Homepage'
-import './App.css'
+import About from './pages/About/About'
+import Episodes from './pages/Episodes/Episodes'
+
+
 
 function App() {
 
 
   return (
-    <div>
+    <BrowserRouter>
       <Header />
-      <Homepage />
+      
+      <Routes>
+        <Route path='/' element={<Homepage />}/>
+        <Route path='/about' element={<About />}/>
+        <Route path='/episodes' element={<Episodes />}/>
+      </Routes>
+      
       <Footer />
-    </div>
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,35 @@
+import React, {useState} from 'react'
+import axios from 'axios'
+import './Search.css'
+
+function Search({setCharacters}) {
+    //need state to hold user input
+    const [query, setQuery] = useState('')
+
+    //https://rickandmortyapi.com/api/character/?name=rick
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        //make api call to get matchig characters to query
+        axios.get(`https://rickandmortyapi.com/api/character/?name=${query}`)
+            .then(res => setCharacters(res.data.results))
+            .catch(err => {
+                if(err.response.status === 404) {
+                    alert(`No characters names ${query}`)
+                }
+                console.log(err)
+            })
+        //clear text box
+        setQuery('')
+        e.target = query
+    }
+
+    return (
+        <form action="" className='search-container' onSubmit={handleSubmit}>
+            <input type="text" onChange={(e) => setQuery(e.target.value)}
+            placeholder='Search all characters' />
+        </form>
+    )
+}
+
+export default Search
